b5d6394698a59c8ebf4c619209fc34886513b184
fangping <pingfang_dlut@163.com>
2020-09-12 15:06:23 +0800

    再次上传

--- src/com/fangping/pro/MyTest.java
+++ src/com/fangping/pro/MyTest.java
@@ -2,6 +2,6 @@ package com.fangping.pro;
 
 public class MyTest {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world every one");
     }
 }

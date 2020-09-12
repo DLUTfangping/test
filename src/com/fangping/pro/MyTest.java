f39d379190f3a8276045d8b34db3623cd97c61d6
fangping <pingfang_dlut@163.com>
2020-09-12 15:13:25 +0800

    test

--- src/com/fangping/pro/MyTest.java
+++ src/com/fangping/pro/MyTest.java
@@ -3,7 +3,7 @@ package com.fangping.pro;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("hello world every one");
-        System.out.println("hello world every one, my name is liam");
+        System.out.println("hello world every one, my name is liam1，kk");
 
     }
 }

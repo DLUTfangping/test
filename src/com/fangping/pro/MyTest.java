5372d2a813fa59e4718b6577f268e657c895b7e3
fangping <pingfang_dlut@163.com>
2020-09-15 23:05:53 +0800

    we are one

--- src/com/fangping/pro/MyTest.java
+++ src/com/fangping/pro/MyTest.java
@@ -4,6 +4,7 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("hello world every one");
         System.out.println("hello world every one, my name is liam1，kk, we are one");
-
+        Test1 test1 = new Test1(1,"2");
+        System.out.println(test1.toString());
     }
 }

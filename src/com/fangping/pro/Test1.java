38032e5fce2606945fdf87de167b9e5328379ea6
fangping <pingfang_dlut@163.com>
2020-09-15 23:09:22 +0800

    we are one

---
+++ src/com/fangping/pro/Test1.java
@@ -0,0 +1,13 @@
+package com.fangping.pro;
+
+public class Test1 {
+    private int age;
+    private String name;
+
+    public Test1(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+
+}
